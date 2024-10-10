af5a6b76757302bdca40666024f29bd284000020
Diana Turluev <dianakolo888@gmail.com>
2024-10-10 21:59:48 +0200

    modifications aussi

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,5 +2,15 @@ from olympics.__main__ import main
 
 
 def test_countries():
-    argv = ['countries']
+    argv = ["countries"]
+    main(argv)
+
+
+def test_individual():
+    argv = ["individual"]
+    main(argv)
+
+
+def test_collective():
+    argv = ["collective"]
     main(argv)
