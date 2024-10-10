edf1b6f2c77b32867c37965b527d7539e5bd38c1
Diana Turluev <dianakolo888@gmail.com>
2024-10-10 18:07:55 +0200

    Test fichier db

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,71 +1,66 @@
 from olympics import db
 
 
-def get_athletes(id=None):
-    rows = db.get_athletes()
-    assert len(rows) > 100
-
-
 def test_countries():
     rows = db.get_countries()
-    assert len(rows) > 69
+    assert len(rows) > 150
+    rows = db.get_countries(1)
+    assert len(rows) == 1
 
+def test_athletes():
+    rows = db.get_athletes()
+    assert len(rows) > 150
+    rows = db.get_athletes(1)
+    assert len(rows) == 1
 
-def get_connection():
-    rows = db.get_connecetions()
+def test_disciplines():
+    rows = db.get_disciplines()
     assert len(rows) > 50
+    rows = db.get_disciplines(1)
+    assert len(rows) == 1
 
-
-def get_disciplines(id=None):
-    rows = db.get_discipilines()
-    assert len(rows) > 1
-
-
-def get_teams(id=None):
+def test_teams():
     rows = db.get_teams()
-    assert len(rows) > 100
+    assert len(rows) > 150
+    rows = db.get_teams(1)
+    assert len(rows) == 1
 
-
-def get_events(id=None):
+def test_events():
     rows = db.get_events()
-    assert len(rows) > 85
+    assert len(rows) > 150
+    rows = db.get_events(1)
+    assert len(rows) == 1
 
-
-def get_medals(id=None):
+def test_medals():
     rows = db.get_medals()
-    assert len(rows) > 100
+    assert len(rows) > 150
+    rows = db.get_medals(1)
+    assert len(rows) == 1
 
+def test_discipline_athletes():
+    rows = db.get_discipline_athletes(1)
+    assert len(rows) >= 15
 
-def get_discipline_athletes(discipline_id):
-    rows = db.get_discipline_athletes()
-    assert len(rows) > 20
-
-
-def get_top_countries(top=10):
+def test_top_countries():
     rows = db.get_top_countries()
-    assert len(rows) > 96
+    assert len(rows) >= 15
 
-
-def get_collective_medals(team_id=None):
+def test_collective_medals():
     rows = db.get_collective_medals()
-    assert len(rows) > 3
+    assert len(rows) >= 15
 
-
-def get_top_collective(top=10):
+def test_top_collective():
     rows = db.get_top_collective()
-    assert len(rows) > 10
+    assert len(rows) >= 15
 
-
-def get_individual_medals(athlete_id=None):
+def test_individual_medals():
     rows = db.get_individual_medals()
-    assert len(rows) > 15
+    assert len(rows) >= 15
+    rows = db.get_individual_medals(1)
+    assert len(rows) == 1
 
-
-def get_top_individual(top=10):
+def test_top_individual():
     rows = db.get_top_individual()
-    assert len(rows) > 100
-
-
-def get_athletes(id=None):
-    rows = db.get_top_individual()
-    assert len(rows) > 100
+    assert len(rows) >= 15
+﻿
+ 
