7b219d84b5b5d18a88cadf2beea8367741c77c91
Diana Turluev <dianakolo888@gmail.com>
2024-10-10 18:08:52 +0200

    Test fichier db

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,11 +7,13 @@ def test_countries():
     rows = db.get_countries(1)
     assert len(rows) == 1
 
+
 def test_athletes():
     rows = db.get_athletes()
     assert len(rows) > 150
     rows = db.get_athletes(1)
     assert len(rows) == 1
+
 
 def test_disciplines():
     rows = db.get_disciplines()
@@ -19,11 +21,13 @@ def test_disciplines():
     rows = db.get_disciplines(1)
     assert len(rows) == 1
 
+
 def test_teams():
     rows = db.get_teams()
     assert len(rows) > 150
     rows = db.get_teams(1)
     assert len(rows) == 1
+
 
 def test_events():
     rows = db.get_events()
@@ -31,27 +35,33 @@ def test_events():
     rows = db.get_events(1)
     assert len(rows) == 1
 
+
 def test_medals():
     rows = db.get_medals()
     assert len(rows) > 150
     rows = db.get_medals(1)
     assert len(rows) == 1
 
+
 def test_discipline_athletes():
     rows = db.get_discipline_athletes(1)
     assert len(rows) >= 15
+
 
 def test_top_countries():
     rows = db.get_top_countries()
     assert len(rows) >= 15
 
+
 def test_collective_medals():
     rows = db.get_collective_medals()
     assert len(rows) >= 15
 
+
 def test_top_collective():
     rows = db.get_top_collective()
     assert len(rows) >= 15
+
 
 def test_individual_medals():
     rows = db.get_individual_medals()
@@ -59,8 +69,7 @@ def test_individual_medals():
     rows = db.get_individual_medals(1)
     assert len(rows) == 1
 
+
 def test_top_individual():
     rows = db.get_top_individual()
     assert len(rows) >= 15
-﻿
- 
